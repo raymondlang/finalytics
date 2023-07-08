2c1538269db46235164318d5e46c2edd04aed4e1
Raymond <raymond.w.lang@gmail.com>
2023-07-08 16:13:26 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Routes } from "react-router-dom";
+import Navbar from "./scenes/navbar";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding=" 1rem 2 rem 4 rem 2 rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

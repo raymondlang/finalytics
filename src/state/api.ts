ae5b4763adef71e6a4b23c4564260f6fb92cc85d
Raymond <raymond.w.lang@gmail.com>
2023-07-13 15:33:51 -0700

    imported products types

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse } from "./types";
+import { GetKpisResponse, GetProductsResponse } from "./types";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),

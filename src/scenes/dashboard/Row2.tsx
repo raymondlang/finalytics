26256630446a7283330c735b8e8d18d95c4baa75
Raymond <raymond.w.lang@gmail.com>
2023-07-13 16:01:55 -0700

    inserted boilerplate pie chart from recharts

--- src/scenes/dashboard/Row2.tsx
+++ src/scenes/dashboard/Row2.tsx
@@ -88,7 +88,47 @@ const Row2 = (props: Props) => {
           </LineChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="e">
+        {" "}
+        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+          <Pie
+            data={data}
+            cx={120}
+            cy={200}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={5}
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Pie
+            data={data}
+            cx={420}
+            cy={200}
+            startAngle={180}
+            endAngle={0}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={5}
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
   );

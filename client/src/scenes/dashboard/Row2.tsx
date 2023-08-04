159aebdc36102efff0b5b98efa7d1e8347e3c8d5
Raymond <raymond.w.lang@gmail.com>
2023-08-04 10:28:42 -0700

    fixed formatting

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,7 +1,10 @@
 import BoxHeader from "../../components/BoxHeader";
 import DashboardBox from "../../components/DashboardBox";
 import FlexBetween from "../../components/FlexBetween";
-import { useGetKpisQuery, useGetProductsQuery } from "../../state/api";
+import {
+  useGetKpisQuery,
+  // useGetProductsQuery
+} from "../../state/api";
 import { Box, Typography, useTheme } from "@mui/material";
 import { useMemo } from "react";
 import {
@@ -118,7 +121,7 @@ const Row2 = () => {
               paddingAngle={2}
               dataKey="value"
             >
-              {pieData.map((entry, index) => (
+              {pieData.map((_, index) => (
                 <Cell key={`cell-${index}`} fill={pieColors[index]} />
               ))}
             </Pie>
